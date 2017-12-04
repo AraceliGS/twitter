f3902b97aac54898fb35a7277fcb1ca4a87403ce
AraceliGS <aragutarras@gmail.com>
2017-12-03 23:39:19 -0500

    añadiendo evento keydown al textarea

--- js/app.js
+++ js/app.js
@@ -3,7 +3,21 @@ window.addEventListener('load', function() {
   var tweetArea = document.getElementById('tweet-area'); // Variable que encierra al elemento textarea
   var tweetBtn = document.getElementById('tweet-btn'); // Variable que encierra al elemento input
   var messages = document.getElementById('messages'); // Variable que encierra al elemento div que se encuentra en section
+  var countDinamic = document.getElementById('count');
+  var MAXCHARACTERS = 140;
 
+  tweetArea.addEventListener('keydown', function(event) {
+    debugger;
+    if (event.target.value.trim().length) { // trim() muestra todos los caracteres escritos en el contenido del textarea, excepto espacio o si no se ha escrito nada
+      var total = MAXCHARACTERS - event.target.value.trim().length;
+      tweetBtn.disabled = false;
+      countDinamic.textContent = MAXCHARACTERS - event.target.value.trim().length;
+    } else {
+      tweetBtn.disabled = true;
+      countDinamic.textContent = MAXCHARACTERS;
+      console.log('la cadena esta vacia');
+    }
+  });
   tweetBtn.addEventListener('click', function(event) {
     event.preventDefault(); // Esto em permite visualizar el valor del textarea en la consola haciendo que no desaparezca 
     // console.log(tweetArea.value); ---> Lo utilice para comprobar si con esto puedo obtener lo escrito en el textarea al hacer click        
@@ -18,7 +32,9 @@ window.addEventListener('load', function() {
     div.classList.add('tweet');
     messages.insertBefore(div, messages.firstElementChild); // si hay algún nodo elemento en el div con el atributo id messages entonces este nuevo nodo se insertará antes que él. De lo contrario, será el primer elemento.
 
-    // Esto me sirve para borrar el contenido qu quede en mi textarea después de escribir y enviar mi tweet
-    tweetArea.value = '';
+    tweetArea.value = ''; // Esto me sirve para borrar el contenido que quede en mi textarea después de escribir y enviar mi tweet
+    tweetArea.focus(); 
+    tweetBtn.disabled = true;
+    countDinamic.textContent = MAXCHARACTERS;
   });
 });
