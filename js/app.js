5468df0e1829091fba4196edca234cd37112a44c
AraceliGS <aragutarras@gmail.com>
2017-12-03 21:54:41 -0500

    añadiendo estilos al body y al div creado para contenr los tweets

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 /* Código */
+window.addEventListener('load', function() {
+  var tweetArea = document.getElementById('tweet-area'); // Variable que encierra al elemento textarea
+  var tweetBtn = document.getElementById('tweet-btn'); // Variable que encierra al elemento input
+  var messages = document.getElementById('messages'); // Variable que encierra al elemento div que se encuentra en section
+
+  tweetBtn.addEventListener('click', function(event) {
+    event.preventDefault(); // Esto em permite visualizar el valor del textarea en la consola haciendo que no desaparezca 
+    // console.log(tweetArea.value); ---> Lo utilice para comprobar si con esto puedo obtener lo escrito en el textarea al hacer click        
+    // Creando nodos donde se mostrará lo escrito en el textarea
+    var div = document.createElement('div');
+    var tweet = document.createElement('span');
+    var tweetText =document.createTextNode(tweetArea.value);
+    // Anexando nodos
+    tweet.appendChild(tweetText);
+    div.appendChild(tweet);
+    div.classList.add('nuevo-mensaje');
+    messages.insertBefore(div, messages.firstElementChild); // si hay algun nodo elemento en el div con el atributo id messages entonces este nuevo nodo se insertará antes que él. De lo contrario, será el primer elemento.
+  });
+});
